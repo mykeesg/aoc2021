15588da0a5cb3df7766b800795e5e4096633e5c2
Miklós Szabó <mykee.sg@gmail.com>
2021-12-01 14:45:21 +0100

    D1a is just D1b with ws 1.

--- Day1.java
+++ Day1.java
@@ -4,47 +4,22 @@ import java.io.FileReader;
 public class Day1 extends Base {
     @Override
     public void runFirst() throws Throwable {
-        try (BufferedReader reader = new BufferedReader(new FileReader("day1.txt"))) {
-            int count = 0;
-            String line = reader.readLine();
-            if (line != null) {
-                int current = Integer.parseInt(line);
-                line = reader.readLine();
-
-                while (line != null) {
-                    int prev = current;
-                    current = Integer.parseInt(line);
-                    if (prev < current) {
-                        ++count;
-                    }
-                    line = reader.readLine();
-                }
-
-            }
-            System.out.println(String.format("Total increments: %1$s", count));
-        }
-    }
-
-    private int sum(int[] array) {
-        int total = 0;
-        for (int i : array) {
-            total += i;
-        }
-        return total;
+        System.out.println(String.format("Total increments: %1$s", solve(1)));
     }
 
     @Override
     public void runSecond() throws Throwable {
-        /*
-         * A different solution could be a list/queue with length 3, where the first
-         * element is always popped, and the new one is instered to the end.
-         * With that, indexing would not be a problem, but I wanted to avoid
-         * unneccessary (un)boxings of int/Integers, I went with the array one.
-         */
+        System.out.println(String.format("Total increments: %1$s", solve(3)));
+    }
 
+    private int solve(int windowSize) throws Throwable {
         try (BufferedReader reader = new BufferedReader(new FileReader("day1.txt"))) {
-
-            int windowSize = 3;
+            /*
+             * A different solution could be a list/queue with the specified length, where
+             * the first element is always popped, and the new one is instered to the end.
+             * With that, indexing would not be a problem, but I wanted to avoid
+             * unneccessary (un)boxings of int/Integers, I went with the array one.
+             */
             int[] window = new int[windowSize];
             int count = 0;
             String line = reader.readLine();
@@ -70,7 +45,15 @@ public class Day1 extends Base {
                 line = reader.readLine();
                 idx = (idx + 1) % windowSize;
             }
-            System.out.println(String.format("Total increments: %1$s", count));
+            return count;
         }
+    }
+
+    private int sum(int[] array) {
+        int total = 0;
+        for (int i : array) {
+            total += i;
+        }
+        return total;
     }
 }
